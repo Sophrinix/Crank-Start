bd380913a63a08f2c7ad270138a84f4f432ff895
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-02-14 11:19:55 -0500

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  ###add associations after making models
+  # has_many :projects
+  # has_many :rewards
+  # has_many :comments
+  # has_many :contributions
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user && user.valid_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base54(16)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end
